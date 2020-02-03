37768ec01dbbc3545e3d08a88c6df949d95bb5aa
Tarek Ali <tarekali15@outlook.com>
2020-02-03 12:02:39 -0600

    working on it

---
+++ app/controllers/app.go
@@ -0,0 +1,17 @@
+package controllers
+
+import (
+	"github.com/revel/revel"
+)
+
+type App struct {
+	*revel.Controller
+}
+
+func (c App) Index() revel.Result {
+	return c.Render()
+}
+
+func (c App) Template_Render() revel.Result {
+	return c.Render()
+}
