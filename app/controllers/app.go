af751cbafaa9801269a530f8c5a0698453e740ae
Tarek Ali <tarekali15@outlook.com>
2020-02-03 18:43:37 -0600

    comments

--- app/controllers/app.go
+++ app/controllers/app.go
@@ -4,14 +4,17 @@ import (
 	"github.com/revel/revel"
 )
 
+// Controller Instance 
 type App struct {
 	*revel.Controller
 }
 
+// home page receiver
 func (c App) Index() revel.Result {
 	return c.RenderTemplate("App/Index.html")
 }
 
+// Article Template receiver
 func (c App) Template_Render() revel.Result {
 	return c.RenderTemplate("App/Template_Render.html")
 }
