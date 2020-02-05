27ac14041c4a3838386e18acc57434ec990a989a
Tarek Ali <tarekali15@outlook.com>
2020-02-05 12:30:26 -0600

    commented out var as it is unused

--- app/controllers/app.go
+++ app/controllers/app.go
@@ -25,7 +25,7 @@ func (c App) ArticleTemplate() revel.Result {
 
 // Article Template data receiver; going to implement model interaction soon
 func (c App) SubmitArticle() revel.Result {
-	text := c.Params.Get("text")
+	// text := c.Params.Get("text")
 	db, err := sql.Open("mysql", "articles")
 	if err != nil {
 		log.Fatalf("database error: %s\n", err)
