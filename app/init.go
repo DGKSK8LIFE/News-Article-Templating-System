f79a04ed87a703d48d3527c649b1f63d151cf8b5
Tarek Ali <tarekali15@outlook.com>
2020-02-06 11:59:50 -0600

    proper tcp argu pre-test

--- app/init.go
+++ app/init.go
@@ -21,7 +21,7 @@ var (
 )
 
 func InitDB() {
-	DB, err = sql.Open("mysql", "username:root@pass1@tcp/articles")
+	DB, err = sql.Open("mysql", "root:pass1@tcp/articles")
 	if err != nil {
 		log.Fatalf("DB Error: %s\n", err)
 	}
