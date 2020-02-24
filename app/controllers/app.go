4bba4964af29e66ceb05b00109bfec9e99badc45
Tarek Ali <tarekali15@outlook.com>
2020-02-24 11:57:56 -0600

    pretest implementation of struct slice

--- app/controllers/app.go
+++ app/controllers/app.go
@@ -35,15 +35,20 @@ func (c App) Index() revel.Result {
 func (c App) Search(query string) revel.Result {
 	articles := []Article{}
 	wildcardQuery := fmt.Sprintf("SELECT title, content FROM article WHERE title LIKE %'%v'%", query)
-	results, _ := app.DB.Query(wildcardQuery)
+	results, err := app.DB.Query(wildcardQuery)
 	if results == nil {
 		c.ViewArgs["message"] = "No matching results"
 		return c.RenderTemplate("App/SearchResults.html")
 	}
-	for _, i := range results {
-		articles = append(articles, i)
+	for results.Next() {
+		article := Article{}
+		err := results.Scan(&article.Title, &article.Content)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		articles = append(articles, article)
 	}
-	message := fmt.Sprintf("Results for search: %s\n", query)
+	message := fmt.Sprintf("Results for search %s:\n", query)
 	c.ViewArgs["message"] = message
 	c.ViewArgs["articles"] = articles
 	return c.RenderTemplate("App/SearchResults.html")
