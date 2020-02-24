2a93a6a175698b48b465fca53f569cfadd1c96de
Tarek Ali <tarekali15@outlook.com>
2020-02-24 11:59:24 -0600

    blank identified errors

--- app/controllers/app.go
+++ app/controllers/app.go
@@ -35,7 +35,7 @@ func (c App) Index() revel.Result {
 func (c App) Search(query string) revel.Result {
 	articles := []Article{}
 	wildcardQuery := fmt.Sprintf("SELECT title, content FROM article WHERE title LIKE %'%v'%", query)
-	results, err := app.DB.Query(wildcardQuery)
+	results, _ := app.DB.Query(wildcardQuery)
 	if results == nil {
 		c.ViewArgs["message"] = "No matching results"
 		return c.RenderTemplate("App/SearchResults.html")
