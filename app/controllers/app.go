dcfbefbab973f48b1ab69ae9a27fe7bf4928f946
Tarek Ali <tarekali15@outlook.com>
2020-03-03 07:24:11 -0600

    working javascript escape

--- app/controllers/app.go
+++ app/controllers/app.go
@@ -106,7 +106,7 @@ func (c App) GetArticle(id int, title ...string) revel.Result {
 		return c.Render()
 	}
 	c.ViewArgs["title"] = article.Title
-	c.ViewArgs["text"] = template.JsEscapeString(string(markdown.ToHTML([]byte(article.Content), nil, nil)))
+	c.ViewArgs["text"] = template.JSEscapeString(string(markdown.ToHTML([]byte(article.Content), nil, nil)))
 	return c.RenderTemplate("App/Post.html")
 }
 
